1d27134a032bf175f0b6c9d0dcbe38e604af14a3
deadphoenix8091 <sschnitzler1994+deadphoeniy8091@gmail.com>
2021-12-20 00:01:59 +0100

    ftp deployment

--- plugins/penya_format.js
+++ plugins/penya_format.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
 Vue.filter('penya', function (value) {
-  // tanks @li-x for his simple formating function
   return Math.round(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' penya'
-  // you can add something like .replace(/,/g, ' ') after toLocaleString method to customize your formatted number
 })
