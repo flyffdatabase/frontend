37b395ba6c7eac3fa0019b7d85fd01eee3964e15
deadphoenix8091 <sschnitzler1994+deadphoeniy8091@gmail.com>
2021-12-28 21:13:29 +0100

    add images to home categories

--- pages/index.vue
+++ pages/index.vue
@@ -4,29 +4,29 @@
     <p>Free online database for the soon to be released game Flyff - Project M.</p>
 
     <div class="row">
-      <nuxt-link to="/items" class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
+      <nuxt-link to="/items" class="col-xl-3 col-md-6">
+          <div class="card border-left-primary shadow">
               <div class="card-body">
                   <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                          <div class="text-xs font-weight-bold text-primary text-uppercase">
                               Item database</div>
                           <div class="h5 mb-0 font-weight-bold text-gray-800">{{ itemCount | thousands }} items</div>
                       </div>
                       <div class="col-auto">
-                          <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                          <img style="width: 4em; height: 4em" src="https://ik.imagekit.io/flyffdb/icon/item/syssysscrsmepro.png"/>
                       </div>
                   </div>
               </div>
           </div>
       </nuxt-link>
 
-      <nuxt-link to="/npcs" class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
+      <nuxt-link to="/npcs" class="col-xl-3 col-md-6">
+          <div class="card border-left-primary shadow">
               <div class="card-body">
                   <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                          <div class="text-xs font-weight-bold text-primary text-uppercase">
                               NPC Database</div>
                           <div class="h5 mb-0 font-weight-bold text-gray-800">{{ npcCount }} npcs</div>
                       </div>
@@ -38,12 +38,12 @@
           </div>
       </nuxt-link>
 
-      <nuxt-link to="/skills" class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
+      <nuxt-link to="/skills" class="col-xl-3 col-md-6">
+          <div class="card border-left-primary shadow">
               <div class="card-body">
                   <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                          <div class="text-xs font-weight-bold text-primary text-uppercase">
                               Skill Database</div>
                           <div class="h5 mb-0 font-weight-bold text-gray-800">{{ skillCount }} skills</div>
                       </div>
@@ -55,12 +55,12 @@
           </div>
       </nuxt-link>
 
-      <nuxt-link to="/quests" class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
+      <nuxt-link to="/quests" class="col-xl-3 col-md-6">
+          <div class="card border-left-primary shadow">
               <div class="card-body">
                   <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                          <div class="text-xs font-weight-bold text-primary text-uppercase">
                               Quest Database</div>
                           <div class="h5 mb-0 font-weight-bold text-gray-800">{{ questCount }} quests</div>
                       </div>
@@ -72,29 +72,29 @@
           </div>
       </nuxt-link>
 
-      <nuxt-link to="/monsters" class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
+      <nuxt-link to="/monsters" class="col-xl-3 col-md-6">
+          <div class="card border-left-primary shadow">
               <div class="card-body">
                   <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                          <div class="text-xs font-weight-bold text-primary text-uppercase">
                               Monster Database</div>
                           <div class="h5 mb-0 font-weight-bold text-gray-800">{{ monsterCount }} monsters</div>
                       </div>
                       <div class="col-auto">
-                          <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                          <img style="width: 4em; height: 4em" src="https://ik.imagekit.io/flyffdb/icon/monster/pukepuke.png?tr=w-100,h-100,cm-pad_resize"/>
                       </div>
                   </div>
               </div>
           </div>
       </nuxt-link>
 
-      <nuxt-link to="/achievements" class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
+      <nuxt-link to="/achievements" class="col-xl-3 col-md-6">
+          <div class="card border-left-primary shadow">
               <div class="card-body">
                   <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                          <div class="text-xs font-weight-bold text-primary text-uppercase">
                               Achievement Database</div>
                           <div class="h5 mb-0 font-weight-bold text-gray-800">{{ achievementCount }} achievements</div>
                       </div>
