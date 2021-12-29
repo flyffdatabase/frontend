57fc02f8cbfccf221bcc4af8cfa8547ca3682ec7
deadphoenix8091 <sschnitzler1994+deadphoeniy8091@gmail.com>
2021-12-29 17:18:55 +0100

    remove v-switch-case import

--- plugins/thousands_format.js
+++ plugins/thousands_format.js
@@ -1,7 +1,4 @@
 import Vue from 'vue'
-import VSwitch from 'v-switch-case'
-
-Vue.use(VSwitch)
 
 Vue.filter('thousands', function (value) {
   return Math.round(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
