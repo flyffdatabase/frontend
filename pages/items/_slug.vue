00194626798e98c1a3462e6cc6a259b254666b7b
deadphoenix <stefanschnitzler286@gmail.com>
2023-12-22 22:28:42 +0100

    pppuuuhhh

--- pages/items/_slug.vue
+++ pages/items/_slug.vue
@@ -1,153 +1,218 @@
 <template>
-  <div class="pt-3">
-    <div class="row">
-      <div class="col-sm-6">
-        <h1 class="m-0">{{ item.name.en }}
-          <template v-if="item.sex == 'male'"><i class="fas fa-mars"></i></template>
-          <template v-if="item.sex == 'female'"><i class="fas fa-venus"></i></template>
-        </h1>
-      </div><!-- /.col -->
-      <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><nuxt-link to="/">Home</nuxt-link></li>
-          <li class="breadcrumb-item"><nuxt-link to="/items">Items</nuxt-link></li>
-          <li class="breadcrumb-item active">Detail</li>
-        </ol>
-      </div><!-- /.col -->
-    </div>
-    <div class="row">
-      <div class="col-xl-3 col-md-4 mb-1">
-        <div class="card shadow">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="card-title">Item Details</h6>
-            </div>
-            <div class="card-body p-4" :style="`background-image: url('${item.icon}');background-repeat: no-repeat;background-position: center center;background-size: 8em auto;image-rendering: pixelated;min-height: 10em;`">
-                
-            </div>
-            <div class="card-body">
-              <table class="table table-striped borderless">
-                <thead>
-                  <tr><th colspan="2">Data</th></tr>
-                </thead>
-                <tbody>
-                  <template v-if="item.category == 'weapon'">
-                    <tr><td>Atk</td><td>{{ item.minAttack }} - {{ item.maxAttack }}</td></tr>
-                  </template>
-                  <template v-if="item.category == 'armor'">
-                    <tr><td>Def</td><td>{{ item.minDefense }} - {{ item.maxDefense }}</td></tr>
-                  </template>
-                  <tr><td>Buy Price: </td><td>{{ item.sellPrice | penya }}</td></tr>
-                  <tr><td>Sell Price: </td><td>{{ item.sellPrice / 4 | penya }}</td></tr>
-                  <template v-if="item.abilities">
-                    <tr>
-                      <td>Abilities</td>
-                      <td>
-                        <ul class="mb-0 pl-4">
-                          <li v-for="itemability in item.abilities" :key="itemability.parameter">
-                            {{ itemability.parameter | ability }}
+    <div class="pt-3">
+        <div class="row">
+            <div class="col-sm-6">
+                <h1 class="m-0">{{ item.name.en }}
+                    <template v-if="item.sex == 'male'"><i class="fas fa-mars"></i></template>
+                    <template v-if="item.sex == 'female'"><i class="fas fa-venus"></i></template>
+                </h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><nuxt-link to="/">Home</nuxt-link></li>
+                    <li class="breadcrumb-item"><nuxt-link to="/items">Items</nuxt-link></li>
+                    <li class="breadcrumb-item active">Detail</li>
+                </ol>
+            </div><!-- /.col -->
+        </div>
+        <div class="row">
+            <div class="col-xl-3 col-md-4 mb-1">
+                <div class="card shadow">
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 class="card-title">Item Details</h6>
+                    </div>
+                    <div class="card-body p-4"
+                        :style="`background-image: url('${item.icon}');background-repeat: no-repeat;background-position: center center;background-size: 8em auto;image-rendering: pixelated;min-height: 10em;`">
 
-                            <template v-if="itemability.add">
-                              + {{ itemability.add }}<template v-if="itemability.rate">%</template>
-                            </template>
-                            
-                          </li>
-                        </ul>
-                      </td>
-                    </tr>
-                  </template>
-                </tbody> 
-              </table>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped borderless">
+                            <thead>
+                                <tr>
+                                    <th colspan="2">Data</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <template v-if="item.category == 'weapon'">
+                                    <tr>
+                                        <td>Atk</td>
+                                        <td>{{ item.minAttack }} - {{ item.maxAttack }}</td>
+                                    </tr>
+                                </template>
+                                <template v-if="item.category == 'armor'">
+                                    <tr>
+                                        <td>Def</td>
+                                        <td>{{ item.minDefense }} - {{ item.maxDefense }}</td>
+                                    </tr>
+                                </template>
+                                <tr>
+                                    <td>Buy Price: </td>
+                                    <td>{{ item.sellPrice | penya }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Sell Price: </td>
+                                    <td>{{ item.sellPrice / 4 | penya }}</td>
+                                </tr>
+                                <template v-if="item.abilities">
+                                    <tr>
+                                        <td>Abilities</td>
+                                        <td>
+                                            <ul class="mb-0 pl-4">
+                                                <li v-for="itemability in item.abilities" :key="itemability.parameter">
+                                                    {{ itemability.parameter | ability }}
 
-              <template v-if="!item.tradable">
-                <div class="card bg-danger text-white shadow mb-1">
-                  <div class="card-body">
-                    This item can <b>NOT</b> be traded!
-                  </div>
+                                                    <template v-if="itemability.add">
+                                                        + {{ itemability.add }}<template
+                                                            v-if="itemability.rate">%</template>
+                                                    </template>
+
+                                                </li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </template>
+                            </tbody>
+                        </table>
+
+                        <template v-if="!item.tradable">
+                            <div class="card bg-danger text-white shadow mb-1">
+                                <div class="card-body">
+                                    This item can <b>NOT</b> be traded!
+                                </div>
+                            </div>
+                        </template>
+
+                    </div>
                 </div>
-              </template>
-              
+            </div>
+            <div class="col-xl-9 col-md-8 mb-8">
+                <h2>Description</h2>
+                <template v-if="!item.raw_description.en || item.raw_description.en == 'null'">
+                    <div class="card bg-info text-white shadow mb-4">
+                        <div class="card-body">
+                            This item has no description!
+                        </div>
+                    </div>
+                </template>
+                <template v-if="item.raw_description.en && item.raw_description.en != 'null'">
+                    <div class="card bg-info text-white shadow mb-4">
+                        <div class="card-body">
+                            {{ item.raw_description.en }}
+                        </div>
+                    </div>
+                </template>
+
+                <template v-if="quests.length">
+                    <h2>Referenced in Quests</h2>
+                    <div v-for="currentQuest in quests" :key="currentQuest.id">
+                        <nuxt-link :to="`/quests/${currentQuest.flyffdb_meta_id}`">
+                            <div class="callout callout-success">
+                                <h5 class="text-info">
+                                    {{ currentQuest.name.en }}
+                                    <sub class="text-warning">Level {{ currentQuest.minLevel }} - {{ currentQuest.maxLevel
+                                    }}</sub>
+                                </h5>
+                                <p class="text-muted">{{ currentQuest.raw_description.en }}</p>
+                            </div>
+                        </nuxt-link>
+                    </div>
+                </template>
+
+                <template v-if="droppingMonsters.length">
+                    <h2>Obtainable Ingame</h2>
+                    <div class="row">
+                        <template v-if="droppingMonsters.length">
+                            <div class="col-xl-6 col-md-6">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h6 class="card-title">Drop from Monsters</h6>
+                                    </div>
+                                    <div class="card-body p-0" style="max-height: 15em;height:15em; overflow-y: auto">
+                                        <table class="table table-striped">
+                                            <tbody>
+                                                <tr v-for="currentMonster in droppingMonsters" :key="currentMonster.id">
+                                                    <td>
+                                                        <nuxt-link :to="`/monsters/${currentMonster.flyffdb_meta_id}`"
+                                                            class="user-block">
+                                                            <img :src="`${currentMonster.icon}?tr=w-100,h-100,cm-pad_resize`"
+                                                                alt="Monster Icon">
+                                                            <span class="username text-muted">{{ currentMonster.name.en
+                                                            }}</span>
+                                                            <span class="description">Level: {{ currentMonster.level
+                                                            }}</span>
+                                                        </nuxt-link>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </template>
+                    </div>
+                </template>
+
+
+                <h2>Player shops:</h2>
+                <div class="row">
+                    <p v-if="$fetchState.pending">Fetching player shop data...</p>
+                    <p v-else-if="$fetchState.error">Error while fetching player shop data</p>
+                    <template v-if="droppingMonsters.length">
+                        <div class="col-xl-6 col-md-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h6 class="card-title">Drop from Monsters</h6>
+                                </div>
+                                <div class="card-body p-0" style="max-height: 15em;height:15em; overflow-y: auto">
+                                    <table class="table table-striped">
+                                        <tbody>
+                                            <tr v-for="currentMonster in droppingMonsters" :key="currentMonster.id">
+                                                <td>
+                                                    <nuxt-link :to="`/monsters/${currentMonster.flyffdb_meta_id}`"
+                                                        class="user-block">
+                                                        <img :src="`${currentMonster.icon}?tr=w-100,h-100,cm-pad_resize`"
+                                                            alt="Monster Icon">
+                                                        <span class="username text-muted">{{ currentMonster.name.en
+                                                        }}</span>
+                                                        <span class="description">Level: {{ currentMonster.level }}</span>
+                                                    </nuxt-link>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </template>
+                </div>
+
             </div>
         </div>
-      </div>
-      <div class="col-xl-9 col-md-8 mb-8">
-        <h2>Description</h2>
-        <template v-if="!item.raw_description.en || item.raw_description.en == 'null'">
-          <div class="card bg-info text-white shadow mb-4">
-            <div class="card-body">
-              This item has no description!
-            </div>
-          </div>
-        </template>
-        <template v-if="item.raw_description.en && item.raw_description.en != 'null'">
-          <div class="card bg-info text-white shadow mb-4">
-            <div class="card-body">
-              {{ item.raw_description.en }}
-            </div>
-          </div>
-        </template>
-        
-        <template v-if="quests.length">
-          <h2>Referenced in Quests</h2>
-          <div v-for="currentQuest in quests" :key="currentQuest.id">
-            <nuxt-link :to="`/quests/${currentQuest.flyffdb_meta_id}`">
-              <div class="callout callout-success">
-                <h5 class="text-info">
-                  {{ currentQuest.name.en }}
-                  <sub class="text-warning">Level {{ currentQuest.minLevel }} - {{ currentQuest.maxLevel }}</sub>
-                </h5>
-                <p class="text-muted">{{ currentQuest.raw_description.en }}</p>
-              </div>
-            </nuxt-link>
-          </div>
-        </template>
-        
-        <template v-if="droppingMonsters.length">
-          <h2>Obtainable Ingame</h2>
-          <div class="row">
-            <template v-if="droppingMonsters.length">
-              <div class="col-xl-6 col-md-6">
-                <div class="card">
-                  <div class="card-header">
-                    <h6 class="card-title">Drop from Monsters</h6>
-                  </div>
-                  <div class="card-body p-0" style="max-height: 15em;height:15em; overflow-y: auto">
-                    <table class="table table-striped">
-                      <tbody>
-                        <tr v-for="currentMonster in droppingMonsters" :key="currentMonster.id">
-                          <td>
-                            <nuxt-link :to="`/monsters/${currentMonster.flyffdb_meta_id}`" class="user-block">
-                              <img :src="`${currentMonster.icon}?tr=w-100,h-100,cm-pad_resize`" alt="Monster Icon">
-                              <span class="username text-muted">{{ currentMonster.name.en }}</span>
-                              <span class="description">Level: {{ currentMonster.level }}</span>
-                            </nuxt-link>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </template>
-          </div>
-        </template>
-
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  async asyncData ({ $http, params, error }) {
-    const data = await $http.$get(`/api/items/${params.slug}`);
-    
-    return data;
-  },
-  head () {
-    return {
-      title: this.item.name.en,
-      description: this.item.raw_description.en
+    async asyncData({ $http, params, error }) {
+        const { item, quests } = await $http.$get(`/api/items/${params.slug}`);
+
+        return { item, quests };
+    },
+    head() {
+        return {
+            title: this.item.name.en,
+            description: this.item.raw_description.en
+        }
+    },
+    data() {
+        return {
+            droppingMonsters: []
+        }
+    },
+    async fetch() {
+        const { droppingMonsters } = await this.$http.$get(`/api/items/${this.$route.params.slug}`)
+        // `todos` has to be declared in data()
+        this.droppingMonsters = droppingMonsters
     }
-  }
 }
 </script>
--- server-middleware/api.js
+++ server-middleware/api.js
@@ -1,149 +1,173 @@
-const bodyParser = require('body-parser')
-const app = require('express')()
-const { $content } = require('@nuxt/content')
-const { $http } = require('@nuxt/http')
+const bodyParser = require("body-parser");
+const app = require("express")();
+const { $content } = require("@nuxt/content");
+const { $http } = require("@nuxt/http");
 
-app.use(bodyParser.json())
-app.get('/monsters/:monsterId', async (req, res) => {
-  let monsterId = req.params.monsterId;
-  let monster
-  try {
-    monster = await $content('monsters', monsterId).fetch()
-  } catch (e) {
-    return error({ message: 'Monster ' + monsterId + ' not found' })
-  }
-
-  let dropItems = {
-    common: [],
-    uncommon: [],
-    rare: [],
-    veryrare: [],
-    unique: []
-  };
-  for (const currentDrop of monster.drops) {
-    let droppedItem;
-    try {
-      droppedItem = await $content('items', 'item_' + currentDrop.item).fetch();
-      dropItems[droppedItem.rarity].push({
-        item: droppedItem, 
-        dropInfo: currentDrop
-      });
-    } catch (e) {
-      return error({ message: 'Dropped Item ' + currentDrop.item + ' not found' })
-    }
-  }
-
-  res.json({ monster: monster, dropItems: dropItems });
-});
-
-app.get('/gameVersion', async (req, res) => {
-  let gameDataVersion = await $content('gameVersion').fetch();
-  res.json({
-    gameDataVersion: gameDataVersion.gameDataVersion
-  });
-});
-
-app.get('/items/:itemId', async (req, res) => {
-  let itemId = req.params.itemId;
-  
-  let item;
-  try {
-    item = await $content('items', itemId).fetch()
-  } catch (e) {
-    return error({ message: 'Item ' + itemId + ' not found' })
-  }
-
-  let droppingMonsters = [];
-  for (const currentMonsterId of item.flyffdb_dropped_by) {
+app.use(bodyParser.json());
+app.get("/monsters/:monsterId", async (req, res) => {
+    let monsterId = req.params.monsterId;
     let monster;
     try {
-      monster = await $content('monsters', 'monster_' + currentMonsterId).fetch();
-      droppingMonsters.push(monster);
+        monster = await $content("monsters", monsterId).fetch();
     } catch (e) {
-      return error({ message: 'Monster ' + currentMonsterId + ' not found' })
+        return error({ message: "Monster " + monsterId + " not found" });
     }
-  }
 
-  droppingMonsters.sort((a, b) => (a.level > b.level) ? 1 : -1);
+    let dropItems = {
+        common: [],
+        uncommon: [],
+        rare: [],
+        veryrare: [],
+        unique: [],
+    };
+    for (const currentDrop of monster.drops) {
+        let droppedItem;
+        try {
+            droppedItem = await $content("items", "item_" + currentDrop.item).fetch();
+            dropItems[droppedItem.rarity].push({
+                item: droppedItem,
+                dropInfo: currentDrop,
+            });
+        } catch (e) {
+            return error({
+                message: "Dropped Item " + currentDrop.item + " not found",
+            });
+        }
+    }
 
-  var results = $content('quests', { deep: true });
-
-  let quests = await results.fetch();
-
-  quests = quests.filter(function(obj) {
-    if (obj.hasOwnProperty('endNeededItems') && obj.endNeededItems)
-      if (obj.endNeededItems.filter((x) => x.item == item.id).length > 0) return true;
-
-    if (obj.hasOwnProperty('endRemoveItems') && obj.endRemoveItems)
-      if (obj.endRemoveItems.filter((x) => x.item == item.id).length > 0) return true;
-
-    if (obj.hasOwnProperty('endReceiveItems') && obj.endReceiveItems)
-      if (obj.endReceiveItems.filter((x) => x.item == item.id).length > 0) return true;
-
-    return false;
-   });
-   
-  res.json({
-    item,
-    droppingMonsters,
-    quests
-  });
+    res.json({ monster: monster, dropItems: dropItems });
 });
 
-app.all('/itemsByCategory', async (req, res) => {
-  let query = $content('items', { deep: true })
-    .sortBy('id', 'asc')
-  const allItems = await query.fetch();
-  const itemsByCategory = {};
+app.get("/gameVersion", async (req, res) => {
+    let gameDataVersion = await $content("gameVersion").fetch();
+    res.json({
+        gameDataVersion: gameDataVersion.gameDataVersion,
+    });
+});
 
-  for (const currentItem of allItems) {
-    if (!currentItem.subcategory || currentItem.subcategory == 'blinkwing' || currentItem.category == 'scroll') {
-      currentItem.subcategory = currentItem.category;
-      currentItem.category = 'other';
+app.get("/items/:itemId", async (req, res) => {
+    let itemId = req.params.itemId;
+
+    let item;
+    try {
+        item = await $content("items", itemId).fetch();
+    } catch (e) {
+        return error({ message: "Item " + itemId + " not found" });
     }
 
-    if (!itemsByCategory.hasOwnProperty(currentItem.category)) {
-      itemsByCategory[currentItem.category] = {
-        'name': currentItem.category,
-        'subcategories': {}
-      };
+    /*let droppingMonsters = [];
+    for (const currentMonsterId of item.flyffdb_dropped_by) {
+      let monster;
+      try {
+        monster = await $content('monsters', 'monster_' + currentMonsterId).fetch();
+        droppingMonsters.push(monster);
+      } catch (e) {
+        return error({ message: 'Monster ' + currentMonsterId + ' not found' })
+      }
     }
-    if (!itemsByCategory[currentItem.category].subcategories.hasOwnProperty(currentItem.subcategory)) {
-      itemsByCategory[currentItem.category].subcategories[currentItem.subcategory] = {
-        'name': currentItem.subcategory,
-        'items': []
-      };
-    }
-
-    itemsByCategory[currentItem.category].subcategories[currentItem.subcategory].items.push(currentItem);
-  }
-
-  for (const categoryName of Object.keys(itemsByCategory)) {
-    for (const subcategoryName of Object.keys(itemsByCategory[categoryName].subcategories)) {
-      if (categoryName == 'armor' || categoryName == 'weapon') [
-        itemsByCategory[categoryName].subcategories[subcategoryName].items.sort((a, b) => (a.level > b.level) ? -1 : 1)
-      ]
-    }
-  }
-  res.json({ itemsByCategory: itemsByCategory });
-})
-
-app.all('/test', async (req, res) => {
-  let query = $content('items', { deep: true })
-    .sortBy('id', 'asc')
-  const allItems = await query.fetch();
-  let filteredItems = [];
-
-  for (const currentItem of allItems) {
-    if (!currentItem.durationRealTime && currentItem.hasOwnProperty('duration')) {
-      filteredItems.push(currentItem.id);
-      filteredItems.push(currentItem.duration);
-      filteredItems.push(currentItem.name.en);
-    }
-  }
   
+    droppingMonsters.sort((a, b) => (a.level > b.level) ? 1 : -1);*/
 
-  res.json({ itemsByCategory: filteredItems });
-})
+    /*var results = $content('quests', { deep: true });
+  
+    let quests = await results.fetch();
+  
+    quests = quests.filter(function(obj) {
+      if (obj.hasOwnProperty('endNeededItems') && obj.endNeededItems)
+        if (obj.endNeededItems.filter((x) => x.item == item.id).length > 0) return true;
+  
+      if (obj.hasOwnProperty('endRemoveItems') && obj.endRemoveItems)
+        if (obj.endRemoveItems.filter((x) => x.item == item.id).length > 0) return true;
+  
+      if (obj.hasOwnProperty('endReceiveItems') && obj.endReceiveItems)
+        if (obj.endReceiveItems.filter((x) => x.item == item.id).length > 0) return true;
+  
+      return false;
+     });*/
 
-module.exports = app
+    res.json({
+        item,
+        droppingMonsters: [],
+        quests: [],
+    });
+});
+
+app.all("/itemsByCategory", async (req, res) => {
+    let query = $content("items", { deep: true }).sortBy("id", "asc");
+    const allItems = await query.fetch();
+    const itemsByCategory = {};
+
+    for (const currentItem of allItems) {
+        if (
+            !currentItem.subcategory ||
+            currentItem.subcategory == "blinkwing" ||
+            currentItem.category == "scroll"
+        ) {
+            currentItem.subcategory = currentItem.category;
+            currentItem.category = "other";
+        }
+
+        if (!itemsByCategory.hasOwnProperty(currentItem.category)) {
+            itemsByCategory[currentItem.category] = {
+                name: currentItem.category,
+                subcategories: {},
+            };
+        }
+        if (
+            !itemsByCategory[currentItem.category].subcategories.hasOwnProperty(
+                currentItem.subcategory
+            )
+        ) {
+            itemsByCategory[currentItem.category].subcategories[
+                currentItem.subcategory
+            ] = {
+                name: currentItem.subcategory,
+                items: [],
+            };
+        }
+
+        itemsByCategory[currentItem.category].subcategories[
+            currentItem.subcategory
+        ].items.push(currentItem);
+    }
+
+    for (const categoryName of Object.keys(itemsByCategory)) {
+        for (const subcategoryName of Object.keys(
+            itemsByCategory[categoryName].subcategories
+        )) {
+            itemsByCategory[categoryName].subcategories[
+                subcategoryName
+            ].items = itemsByCategory[categoryName].subcategories[
+                subcategoryName
+            ].items.slice(0, 10);
+            if (categoryName == "armor" || categoryName == "weapon")
+                [
+                    itemsByCategory[categoryName].subcategories[
+                        subcategoryName
+                    ].items.sort((a, b) => (a.level > b.level ? -1 : 1)),
+                ];
+        }
+    }
+    res.json({ itemsByCategory: itemsByCategory });
+});
+
+app.all("/test", async (req, res) => {
+    let query = $content("items", { deep: true }).sortBy("id", "asc");
+    const allItems = await query.fetch();
+    let filteredItems = [];
+
+    for (const currentItem of allItems) {
+        if (
+            !currentItem.durationRealTime &&
+            currentItem.hasOwnProperty("duration")
+        ) {
+            filteredItems.push(currentItem.id);
+            filteredItems.push(currentItem.duration);
+            filteredItems.push(currentItem.name.en);
+        }
+    }
+
+    res.json({ itemsByCategory: filteredItems });
+});
+
+module.exports = app;
